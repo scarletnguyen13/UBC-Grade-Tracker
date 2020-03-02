2f9780031fbdc7df91263a2aa4746485150ac1fa
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-03-02 13:24:19 -0800

    Add GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,26 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        new GradeTracker();
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox(new Label("A JavaFX Label"));
+        Scene scene = new Scene(vbox);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX Stage Window Title");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
+        primaryStage.show();
     }
 }
